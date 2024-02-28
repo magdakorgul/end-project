df3b59441f702fcc3c84546a0b784d7f5fa4fd85
magdakorgul <magdalenakorgul1@gmail.com>
2024-02-28 17:15:57 +0100

    Added a footer with a link to my code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>This project was coded by Magdalena Korgul and is 
+      <a href='https://github.com/magdakorgul/end-project' target='_blank'> open-sourced on GitHub</a>
+    </footer>
     </div>
   );
 }
