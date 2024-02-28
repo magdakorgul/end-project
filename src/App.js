345c99ee531199179fdfe7d0b1320c9ca6b45b50
magdakorgul <magdalenakorgul1@gmail.com>
2024-02-28 17:19:48 +0100

    Added a H2 test copy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <h2>Test test test</h2>
       <footer>This project was coded by Magdalena Korgul and is 
       <a href='https://github.com/magdakorgul/end-project' target='_blank'> open-sourced on GitHub</a>
     </footer>
