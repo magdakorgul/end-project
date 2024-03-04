ea9ad87bd9d085d6a6948db443f18b4c0efb7b68
magdakorgul <magdalenakorgul1@gmail.com>
2024-03-04 16:34:56 +0100

    1st deployment test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
       <h1>Weather App</h1>
-      <h2>Test test test</h2>
+      <Weather />
       <footer>This project was coded by Magdalena Korgul and is 
       <a href='https://github.com/magdakorgul/end-project' target='_blank'> open-sourced on GitHub</a>
     </footer>
