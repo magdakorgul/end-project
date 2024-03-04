7f8448fa0117a090d9853b74fb32b364d46972d6
magdakorgul <magdalenakorgul1@gmail.com>
2024-03-04 18:08:26 +0100

    Created basic HTML code for the project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
       <Weather />
       <footer>This project was coded by Magdalena Korgul and is 
       <a href='https://github.com/magdakorgul/end-project' target='_blank'> open-sourced on GitHub</a>
