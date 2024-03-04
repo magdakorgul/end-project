02feda8a35fae1501ab25063237d1f712525e30f
magdakorgul <magdalenakorgul1@gmail.com>
2024-03-04 12:04:47 +0100

    Fixed mistakes in the terminal

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <h2>Test test test</h2>
       <footer>This project was coded by Magdalena Korgul and is 
       <a href='https://github.com/magdakorgul/end-project' target='_blank'> open-sourced on GitHub</a>
     </footer>
+    </div>
     </div>
   );
 }
