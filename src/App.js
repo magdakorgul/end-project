514e1afa261b33b5fbaebf4e4f43f0c3c9e3d848
magdakorgul <magdalenakorgul1@gmail.com>
2024-03-06 14:09:57 +0100

    Added basic styling in CSS

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
     <div className="App">
       <div className="container">
       <Weather />
-      <footer>This project was coded by Magdalena Korgul and is 
+      
+    </div>
+    <footer>This project was coded by Magdalena Korgul and is 
       <a href='https://github.com/magdakorgul/end-project' target='_blank'> open-sourced on GitHub</a>
     </footer>
-    </div>
     </div>
   );
 }
