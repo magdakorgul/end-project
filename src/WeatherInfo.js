7e086dfa93079d01c6c607215cda1567ded9aa2d
magdakorgul <magdalenakorgul1@gmail.com>
2024-03-20 17:48:27 +0100

    Created a new component and added calculations for converting to C to F

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 
 export default function WeatherInfo(props){
@@ -20,10 +21,11 @@ export default function WeatherInfo(props){
                 <div className="float-left">
                 <WeatherIcon code={props.data.icon} />
                 </div>
-            
-         <span className="temperature">{Math.round(props.data.temperature)}</span>
-        <span className="unit">°C</span>
+           <div className="float-left">
+            <WeatherTemperature celsius={props.data.temperature}/>
+         
             </div>
+            </div> 
             
             <div className="col-6">
                 <ul>
